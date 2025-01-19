cb75c53ba3a6d2ef3fb8da1b71b5611d5a4d2187
ZamniProg <artemopbgsg@gmail.com>
2025-01-19 19:18:14 +0300

    Install update

--- imports.py
+++ imports.py
@@ -1,17 +1,20 @@
 import os
 import json
 import shutil
-import datasets
-import numpy as np
-import tensorflow as tf
-from datasets import DatasetDict
-from keras.src.legacy.backend import update
 from tqdm import tqdm
-from PIL import Image
+
+import datasets
+from datasets import DatasetDict
+
 from matplotlib import pyplot as plt
+import numpy as np
 from numpy.typing import NDArray
+
+from PIL import Image
 from pycocotools.coco import COCO
+
 from typing import Union, List, Tuple, Dict, Iterable
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
